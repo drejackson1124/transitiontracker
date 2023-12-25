dd44a287ac475c72ed5929968f385e90eb0cb5db
Andre Jackson <andrejackson@Andres-MacBook-Air.local>
2023-12-25 13:01:15 -0600

    add dashboard layout

---
+++ src/layouts/dashboard.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Dashboard = (props) => {
+    console.log(props.user);
+    return (
+        <h1>hello, I am dashboard</h1>
+    )
+}
+
+export default Dashboard;
